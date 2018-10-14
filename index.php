a5f75cbf264742db8a8d6a1d2058ed83ad3c3c51
Bryan Azofeifa Aguero <bryan@MacBook-Pro-de-Bryan.local>
2018-10-14 17:12:33 -0600

    Add file index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+// Ejemplo de conexión a base de datos MySQL con PHP.
+	//
+	// Ejemplo realizado por Oscar Abad Folgueira: http://www.oscarabadfolgueira.com y https://www.dinapyme.com
+	
+	// Datos de la base de datos
+	$usuario = "root";
+	$password = "root";
+	$servidor = "localhost";
+	$basededatos = "games";
+	
+	// creación de la conexión a la base de datos con mysql_connect()
+	$conexion = mysqli_connect( $servidor, $usuario, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
+	
+	// Selección del a base de datos a utilizar
+	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+	// establecer y realizar consulta. guardamos en variable.
+    $consulta = "SELECT * FROM lista_juegos";
+    $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+    $columna = mysqli_fetch_array( $resultado );
+    print_r($columna);
+?>
